adb00392a502da8c5538b4e5e0bb57efaa36af47
Daniel Araujo <daniel@kuriate.com.br>
2021-01-08 20:10:37 -0300

    Initial application

---
+++ app/views.py
@@ -0,0 +1,90 @@
+from flask import render_template, flash, request, redirect, url_for, safe_join, send_file, send_from_directory, abort
+from app import app
+import os
+import uuid
+from werkzeug.utils import secure_filename
+from structural import calculate, make_gtif, make_shp
+
+UPLOAD_FOLDER = 'app\\uploads\\'
+STATIC_FOLDER = 'app\\static\\'
+FULL_STATICPATH = '\\app\\static\\'
+ALLOWED_EXTENSIONS = {'tif'}
+app.config['FULL_STATICPATH'] = FULL_STATICPATH
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STATIC_FOLDER'] = STATIC_FOLDER
+URL = r'http://127.0.0.1:5000'
+
+@app.route('/')  
+def index():  
+    return render_template("index.html") 
+
+@app.route('/faq')  
+def faq():  
+    return render_template("faq.html") 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        for f in request.files.getlist('file'):
+            uuid_fname = str(uuid.uuid1())
+            uuid_fname_tif = uuid_fname + '.tif'
+            f = request.files['file']  
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], uuid_fname_tif))
+    return redirect(url_for('editor', uuid_fname=uuid_fname, usr_nscale=5, usr_norient=6, usr_minWaveLength=3, usr_mult=2.1, usr_sigmaOnf=0.55, usr_k=20.0, usr_polarity=0, usr_noiseMethod=-1))
+
+@app.route('/editor/<uuid_fname>?usr_nscale=<usr_nscale>&usr_norient=<usr_norient>&usr_minWaveLength=<usr_minWaveLength>&usr_mult=<usr_mult>&usr_sigmaOnf=<usr_sigmaOnf>&usr_k=<usr_k>&usr_polarity=<usr_polarity>&usr_noiseMethod=<usr_noiseMethod>', methods=['GET'])  
+def editor(uuid_fname, usr_nscale, usr_norient, usr_minWaveLength, usr_mult, usr_sigmaOnf, usr_k, usr_polarity, usr_noiseMethod):  
+    #if request.method == 'POST':  
+    uuid_fname_tif = uuid_fname + '.tif'
+    uuid_fname_png = uuid_fname + '_fused.png'
+    fimg = os.path.join(app.config['UPLOAD_FOLDER'], uuid_fname_tif)
+    calculate(fimg, uuid_fname, usr_nscale, usr_norient, usr_minWaveLength, usr_mult, usr_sigmaOnf, usr_k, usr_polarity, usr_noiseMethod)
+    return render_template("editor.html", imgdir=uuid_fname_png, id=uuid_fname, usr_nscale=usr_nscale, usr_norient=usr_norient, usr_minWaveLength=usr_minWaveLength, usr_mult=usr_mult, usr_sigmaOnf=usr_sigmaOnf, usr_k=usr_k, usr_polarity=usr_polarity, usr_noiseMethod=usr_noiseMethod)  
+
+@app.route('/refresh_editor', methods=['POST'])
+def refresh_editor():
+    uuid_fname = request.form['uuid_fname']
+    usr_nscale = request.form['usrnscale']
+    usr_norient = request.form['usrnorient']
+    usr_minWaveLength = request.form['usrminWaveLength']
+    usr_mult = request.form['usrmult']
+    usr_sigmaOnf = request.form['usrsigmaOnf']
+    usr_k = request.form['usrk']
+    usr_polarity = request.form['usrpolarity']
+    usr_noiseMethod = request.form['usrnoiseMethod']
+    return redirect(url_for('editor', uuid_fname=uuid_fname, usr_nscale=usr_nscale, usr_norient=usr_norient, usr_minWaveLength=usr_minWaveLength, usr_mult=usr_mult, usr_sigmaOnf=usr_sigmaOnf, usr_k=usr_k, usr_polarity=usr_polarity, usr_noiseMethod=usr_noiseMethod))
+
+
+@app.route('/makepng/<uuid_fname>')
+def return_png(uuid_fname):
+    try:
+        uuid_fname_png = uuid_fname + '_file.png'
+        return render_template("download.html", url=URL, file=uuid_fname_png)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/maketif/<uuid_fname>')
+def return_tif(uuid_fname):
+    try:
+        uuid_fname_gtif = uuid_fname + '_geotiff.tif'
+        make_gtif(uuid_fname)
+        return render_template("download.html", url=URL, file=uuid_fname_gtif)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/makeshp/<uuid_fname>')
+def return_shp(uuid_fname):
+    try:
+        uuid_fname_zip = uuid_fname + '.zip'
+        make_shp(uuid_fname)
+        return render_template("download.html", url=URL, file=uuid_fname_zip)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route('/download/<uuid_fname>', methods=['GET'])
+def return_file(uuid_fname):
+    safe_path = safe_join(app.config["FULL_STATICPATH"], uuid_fname)
+    try:
+	    return send_file(safe_path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
---
+++ structural.py
@@ -0,0 +1,145 @@
+import numpy as np
+import tifffile
+import cv2
+import os
+from zipfile import ZipFile 
+from app import app
+from osgeo import gdal, gdalnumeric, ogr, osr
+from phasetools import phasesym
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import savefig
+from skimage.color import rgb2gray
+from skimage.filters import gaussian, threshold_otsu, rank
+from skimage.filters.rank import entropy
+from skimage.io import imsave, imread
+from skimage.morphology import square
+from skimage.util import img_as_ubyte
+from skimage.morphology import skeletonize
+
+def calculate(imgfile, uuid_fname, usr_nscale, usr_norient, usr_minWaveLength, usr_mult, usr_sigmaOnf, usr_k, usr_polarity, usr_noiseMethod):
+    
+    usr_nscale = int(usr_nscale)
+    usr_norient = int(usr_norient)
+    usr_minWaveLength = int(usr_minWaveLength)
+    usr_mult = float(usr_mult) 
+    usr_sigmaOnf = float(usr_sigmaOnf) 
+    usr_k = float(usr_k) 
+    usr_polarity = int(usr_polarity) 
+    usr_noiseMethod = int(usr_noiseMethod)
+    
+    # Names var for later use
+    STATIC_FOLDER = 'app\\static\\'
+    pngfile = os.path.join(STATIC_FOLDER, uuid_fname + '_file.png') 
+    alignedname = os.path.join(STATIC_FOLDER, uuid_fname + '_align.png') 
+    fusedname = os.path.join(STATIC_FOLDER, uuid_fname + '_fused.png') 
+
+    # Import file
+    data = tifffile.imread(imgfile)
+    
+    # Convert 3-band RGB to 1-band grayscale
+    grayscale = rgb2gray(data)
+
+    # Calculate standard deviation
+    mult_1 = np.multiply(grayscale, grayscale)
+    blur_1 = gaussian(mult_1, sigma=(9, 9), truncate=3.5, multichannel=False)
+    blur_2 = gaussian(grayscale, sigma=(9, 9), truncate=3.5, multichannel=False)
+    mult_2 = np.multiply(blur_2, blur_2)
+    std_hat = np.sqrt(blur_1 - mult_2)
+
+    # Calculate entropy
+    sigma_hat = entropy(std_hat, square(9))
+
+    # Phase symmetry
+    psym = phasesym(sigma_hat, nscale=usr_nscale, norient=usr_norient, minWaveLength=usr_minWaveLength, mult=usr_mult, sigmaOnf=usr_sigmaOnf, k=usr_k, polarity=usr_polarity, noiseMethod=usr_noiseMethod)
+
+    # Threshold
+    selem = square(9)
+    local_otsu = rank.otsu(psym, selem)
+    threshold_global_otsu = threshold_otsu(psym)
+    global_otsu = (psym >= threshold_global_otsu)
+
+    # Skeletonize and invert colors
+    skeleton = skeletonize(global_otsu, method='lee')
+    skeleton[skeleton == 255] = 1
+    skeleton[skeleton == 0] = 255
+    skeleton[skeleton == 1] = 0
+
+    # Save intermediate files as png
+    plt.imsave(pngfile, data)
+    plt.imsave(alignedname, skeleton, cmap=plt.cm.gray)
+
+    # Join both images  for display
+    img1 = cv2.imread(pngfile)
+    img2 = cv2.imread(alignedname)
+
+    rows,cols,channels = img2.shape
+    roi = img1[0:rows, 0:cols ]
+
+    # Now create a mask of logo and create its inverse mask also
+    img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY_INV)
+    mask_inv = cv2.bitwise_not(mask)
+
+    # Now black-out the area of logo in ROI
+    img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+
+    # Take only region of logo from logo image.
+    img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+
+    # Put logo in ROI and modify the main image
+    dst = cv2.add(img1_bg,img2_fg)
+    img1[0:rows, 0:cols ] = dst
+    cv2.imwrite(fusedname, img1)
+
+def make_shp(imgname):
+    UPLOAD_FOLDER = 'app\\uploads\\'
+    STATIC_FOLDER = 'app\\static\\'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    origname = UPLOAD_FOLDER + imgname + '.tif'
+    alignedname = STATIC_FOLDER + imgname + '_align.png'
+    zipname =  STATIC_FOLDER + imgname + '.zip'
+
+    ziplist = [ STATIC_FOLDER + imgname + '_shape.dbf',
+    STATIC_FOLDER + imgname + '_shape.prj',
+    STATIC_FOLDER + imgname + '_shape.shp',
+    STATIC_FOLDER + imgname + '_shape.shx'
+    ]
+
+    source = gdal.Open(origname)
+    alignments = gdal.Open(alignedname)
+    srcband = alignments.GetRasterBand(1)
+    proj = osr.SpatialReference(wkt=source.GetProjection())
+    dst_layername = imgname + "_shape"
+
+    drv = ogr.GetDriverByName("ESRI Shapefile")
+    dst_ds = drv.CreateDataSource( STATIC_FOLDER + dst_layername + ".shp" )
+    dst_layer = dst_ds.CreateLayer(dst_layername, proj )
+    gdal.Polygonize( srcband, None, dst_layer, -1, [], callback=None )
+    dst_ds.FlushCache()
+
+
+    with ZipFile(zipname,'w') as zip: 
+        for file in ziplist:
+            zip.write(file)
+
+def make_gtif(imgname):
+    UPLOAD_FOLDER = 'app\\uploads\\'
+    STATIC_FOLDER = 'app\\static\\'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    origname = os.path.join(app.config['UPLOAD_FOLDER'], imgname + '.tif')
+    fusedname = os.path.join(STATIC_FOLDER, imgname + '_fused.png') 
+    gtif = os.path.join(STATIC_FOLDER, imgname + '_geotiff.tif') 
+
+
+    source = gdal.Open(origname)
+    pngfusion = gdal.Open(fusedname)
+    R = np.array(pngfusion.GetRasterBand(1).ReadAsArray())
+    G = np.array(pngfusion.GetRasterBand(2).ReadAsArray())
+    B = np.array(pngfusion.GetRasterBand(3).ReadAsArray())
+
+    dst_ds = gdal.GetDriverByName('GTiff').Create(gtif, R.shape[1], R.shape[0], 3, gdal.GDT_Byte, options = ['PHOTOMETRIC=RGB', 'PROFILE=GeoTIFF', 'COMPRESS=LZW'])
+    gdalnumeric.CopyDatasetInfo(source, dst_ds)
+    dst_ds.GetRasterBand(1).WriteArray(R)
+    dst_ds.GetRasterBand(2).WriteArray(G)
+    dst_ds.GetRasterBand(3).WriteArray(B)
+    dst_ds.FlushCache()
